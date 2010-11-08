5d2ea2d829df35b380c152b9d00e5b0f24e42b3f
Aaron Parker <ros-vm@ros-vm.(none)>
2010-11-08 12:58:22 -0600

    added coax_client package from previous development

---
+++ coax_client/build/CMakeFiles/test_coax_liftoff.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test_coax_liftoff.dir/src/TestCoaxAutoflight.o"
+  "../bin/test_coax_liftoff.pdb"
+  "../bin/test_coax_liftoff"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test_coax_liftoff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ coax_client/src/TestCoaxSubscriber.cpp
@@ -0,0 +1,21 @@
+#include "ros/ros.h"
+#include "coax_msgs/CoaxState.h"
+#include <iostream>
+
+using namespace std;
+
+void state_subscriber_callback(const coax_msgs::CoaxStateConstPtr& msg)
+{
+	cout << "recieved a state!\nfront ET: " << msg->hranges[0] << endl;
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "test_coax_client");
+	ros::NodeHandle n;
+	
+	ros::Subscriber coax_state_subscriber = n.subscribe("coax_server/state",1,state_subscriber_callback);
+	ros::spin();
+
+	return 0;
+}
