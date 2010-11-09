ea4ccc69dfa7f888410dd18634854bf5c610b477
Aaron Parker <redbaron148@gmail.com>
2010-11-09 12:30:43 -0600

    modified autoflight, works well in the simulator, take off, wait a few seconds, increase altitude, land.

--- coax_client/src/TestCoaxAutoflight.cpp
+++ coax_client/src/TestCoaxAutoflight.cpp
@@ -1,9 +1,11 @@
 #include "ros/ros.h"
 #include "coax_msgs/CoaxState.h"
 #include "coax_msgs/CoaxReachNavState.h"
+#include "coax_msgs/CoaxSetControl.h"
 #include <iostream>
 
 ros::ServiceClient reach_nav_state_client;
+ros::ServiceClient set_control_client;
 
 using namespace std;
 
@@ -17,27 +19,35 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "test_coax_client");
 	ros::NodeHandle n;
 
-	ros::Subscriber coax_state_subscriber = n.subscribe("coax_server/state",1,state_subscriber_callback);
+	//ros::Subscriber coax_state_subscriber = n.subscribe("coax_server/state",1,state_subscriber_callback);
 	reach_nav_state_client = n.serviceClient<coax_msgs::CoaxReachNavState>("coax_server/reach_nav_state");
+	set_control_client = n.serviceClient<coax_msgs::CoaxSetControl>("coax_server/set_control");
 	
 	coax_msgs::CoaxReachNavState nav_state;
-	nav_state.request.desiredState = 1;
-	nav_state.request.timeout = 5;
+	coax_msgs::CoaxSetControl control;
+
+	nav_state.request.desiredState = 5;
+	nav_state.request.timeout = 10;
+
+	control.request.roll = 0;
+	control.request.pitch = 0;
+	control.request.yaw = 0;
+	control.request.altitude = 1.0;
 
 	reach_nav_state_client.call(nav_state);
-	cout << "reach idle, wait 1 second then go to takeoff\n";
+	cout << "reach controlled state, wait 1 second\n";
 
-	usleep(1000000);
+	usleep(100000);
+
+	set_control_client.call(control);
+	cout << "set new control, wait 1 second\n";
+
+	//usleep(10000000);
 
 	nav_state.request.desiredState = 3;
-	reach_nav_state_client.call(nav_state);
-	cout << "reach " << nav_state.request.desiredState  << ", wait 5 seconds then go to stop mode.\n";
 
-	usleep(5000000);
-
-	nav_state.request.desiredState = 0;
 	reach_nav_state_client.call(nav_state);
-	cout << "reach stop mode. end of program\n";
+	cout << "land, end of program.\n";
 
 	//ros::spinOnce();	
 
