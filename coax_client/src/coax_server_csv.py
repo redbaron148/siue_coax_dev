c70ec071a67f50527357d32f35b4e7ff6e354b6a
Aaron <redbaron148@gmail.com>
2010-12-07 12:40:11 -0600

    dumped all test flight .bag files into xls files located in coax_client/data.  Tweaked the coax_bag.launch file, only launches the csv_dump python script.

--- coax_client/src/coax_server_csv.py
+++ coax_client/src/coax_server_csv.py
@@ -6,22 +6,22 @@ from coax_msgs.msg import CoaxState
 data = "time,roll,pitch,yaw,gyro[0],gyro[1],gyro[2],accel[0],accel[1],accel[2]"
 data += ",zrange,zfiltered,pressure,hrange[0],hrange[1],hrange[2],rcChannel[0],"
 data += "rcChannel[1],rcChannel[2],rcChannel[3],rcChannel[4],rcChannel[5],"
-data += "rcChannel[6],rcChannel[7],filtered_accel[0],filtered_accel2[0],filtered_accel3[0]"
+data += "rcChannel[6],rcChannel[7]"
 
-filtered0 = 0
-filtered1 = 0
-filtered2 = 0
+#filtered0 = 0
+#filtered1 = 0
+#filtered2 = 0
 
 filename = "tmp.csv"
 
 def callback(state):
-    rospy.loginfo(rospy.get_name()+"I heard something, adding to data")
-    global filtered0
-    global filtered1
-    global filtered2
-    filtered0 = 0.5*filtered0+0.5*state.accel[0]
-    filtered1 = 0.6*filtered1+0.5*state.accel[0]
-    filtered2 = 0.8*filtered2+0.5*state.accel[0]
+    #rospy.loginfo(rospy.get_name()+"I heard something, adding to data")
+    #global filtered0
+    #global filtered1
+    #global filtered2
+    #filtered0 = 0.5*filtered0+0.5*state.accel[0]
+    #filtered1 = 0.6*filtered1+0.5*state.accel[0]
+    #filtered2 = 0.8*filtered2+0.5*state.accel[0]
     global data
     data += "\n"+`state.header.stamp.to_sec()`+","+`state.roll`+","+`state.pitch`+","+`state.yaw`
     data += ","+`state.gyro[0]`+","+`state.gyro[1]`+","+`state.gyro[2]`
@@ -31,16 +31,18 @@ def callback(state):
     data += ","+`state.rcChannel[0]`+","+`state.rcChannel[1]`+","+`state.rcChannel[2]`
     data += ","+`state.rcChannel[3]`+","+`state.rcChannel[4]`+","+`state.rcChannel[5]`
     data += ","+`state.rcChannel[6]`+","+`state.rcChannel[7]`
-    data += ","+`filtered0`+","+`filtered1`+","+`filtered2`
+    #data += ","+`filtered0`+","+`filtered1`+","+`filtered2`
     
 def listener():
     rospy.init_node('listener', anonymous=True)
-    print "stuff"
     rospy.Subscriber("/coax_server/state", CoaxState, callback)
     rospy.spin()
     global data
+    print "opened file data/tmp.csv\n"
     newfile = open(filename,'w')
+    print "wrote data to file\n"
     newfile.write(data)
+    print "closed file\n"
     newfile.close()
 
 if __name__ == '__main__':
