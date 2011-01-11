c7bee98a491cd1603ce3d190dec6507bdce47c21
Aaron Parker <redbaron148@gmail.com>
2011-01-11 15:16:55 -0600

    deleted old bag data, added color calibration file for coax usb cam

--- coax_client/src/coax_csv.py
+++ coax_client/src/coax_csv.py
@@ -30,20 +30,20 @@ def listener():
     full_filename = path+filename+".csv"
     
     rospy.spin()
-        
+    
     if os.path.isfile(full_filename):
         if overwrite:
             rospy.logwarn("The file %s already exists, overwriting now.", full_filename)
         else:
             full_filename = path+datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S.")+filename+".csv"
             rospy.logwarn("The file already exists, creating new file %s",full_filename)
-        rospy.loginfo("writing data to %s",full_filename)
-        newfile = open(full_filename,'w')
-        newfile.write(data)
-        newfile.close()
-        rospy.loginfo("wrote %d messages",count);
-        count = 0
-        data = '';
+    rospy.loginfo("writing data to %s",full_filename)
+    newfile = open(full_filename,'w')
+    newfile.write(data)
+    newfile.close()
+    rospy.loginfo("wrote %d messages",count);
+    count = 0
+    data = '';
 
 if __name__ == '__main__':
     try:
