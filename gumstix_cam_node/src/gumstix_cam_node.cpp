dbc510f8e596f9d26d13d60e955c9ac06817f8e7
Aaron Parker <redbaron148@gmail.com>
2011-02-22 16:22:00 -0600

    implemented auto white balance as a rosparam

--- gumstix_cam_node/src/gumstix_cam_node.cpp
+++ gumstix_cam_node/src/gumstix_cam_node.cpp
@@ -58,30 +58,31 @@ int main(int argc, char **argv)
     string topic = "image_";
     string device = "/dev/video";
 
-	int camID = 0;
-	int FPS = 10;
-	int height = 480;
-	int width = 640;
-	int hue = 0;
-	int saturation = 0;
-	int contrast = 0;
-	int sharpness = 0;
-	int brightness = 0;
-	
-	nh.param( "v4l_id", camID, int(camID) );
-	nh.param( "fps", FPS, int(FPS));
-	nh.param( "height", height, int(height));
-	nh.param( "width", width, int(width));
-	
-	ROS_INFO( "Using USB camera at V4L index: %d at %d FPS. Image is %d x %d.", camID, FPS , width, height);	
-	//delete params to support multiple instances of this node with different
-	// config options
-	nh.deleteParam( "v4l_id" );
-	nh.deleteParam( "fps" );	
-	
-	//append the V4L id to this topic for multiple camera support
-	topic  += boost::lexical_cast<std::string>(camID);
-	device += boost::lexical_cast<std::string>(camID);
+    int camID = 0;
+    int FPS = 10;
+    int height = 480;
+    int width = 640;
+    int hue = 0;
+    int saturation = 0;
+    int contrast = 0;
+    int sharpness = 0;
+    int brightness = 0;
+    bool auto_white_balance = true;
+    
+    nh.param( "v4l_id", camID, int(camID) );
+    nh.param( "fps", FPS, int(FPS));
+    nh.param( "height", height, int(height));
+    nh.param( "width", width, int(width));
+    
+    ROS_INFO( "Using USB camera at V4L index: %d at %d FPS. Image is %d x %d.", camID, FPS , width, height);    
+    //delete params to support multiple instances of this node with different
+    // config options
+    nh.deleteParam( "v4l_id" );
+    nh.deleteParam( "fps" );    
+    
+    //append the V4L id to this topic for multiple camera support
+    topic  += boost::lexical_cast<std::string>(camID);
+    device += boost::lexical_cast<std::string>(camID);
 
     //setup the publisher
     ros::Publisher cameraPub = n.advertise<sensor_msgs::Image>(topic, 1);
@@ -91,52 +92,58 @@ int main(int argc, char **argv)
     IplImage *rawImg = cvCreateImage(cvSize(width, height), 8, 3);
     int cap_status = 0;
 
-	Camera cap_device( device.c_str(), width, height, FPS );
-	
-	nh.param( "brightness",brightness,int(cap_device.defaultBrightness()));
-	nh.param( "contrast",contrast,int(cap_device.defaultContrast()));
-	nh.param( "saturation",saturation,int(cap_device.defaultSaturation()));
-	nh.param( "sharpness",sharpness,int(cap_device.defaultSharpness()));
-	nh.param( "hue",hue,int(cap_device.defaultHue()));
-	
-	ROS_INFO("brightness: %d  contrast: %d  hue: %d  saturation: %d  sharpness: %d",brightness,contrast,hue,saturation,sharpness);
+    Camera cap_device( device.c_str(), width, height, FPS );
+    
+    nh.param( "brightness",brightness,int(cap_device.defaultBrightness()));
+    nh.param( "contrast",contrast,int(cap_device.defaultContrast()));
+    nh.param( "saturation",saturation,int(cap_device.defaultSaturation()));
+    nh.param( "sharpness",sharpness,int(cap_device.defaultSharpness()));
+    nh.param( "hue",hue,int(cap_device.defaultHue()));
+    nh.param( "auto_white_balance",auto_white_balance,cap_device.isAutoWhiteBalance());
+    
+    ROS_INFO("brightness: %d",brightness);
+    ROS_INFO("contrast: %d",contrast);
+    ROS_INFO("hue: %d",hue);
+    ROS_INFO("saturation: %d",saturation);
+    ROS_INFO("sharpness: %d",sharpness);
+    ROS_INFO("auto white balance: %d",auto_white_balance);
 
-	cap_device.setHue(hue);
-	cap_device.setBrightness(brightness);
-	cap_device.setContrast(contrast);
-	cap_device.setSaturation(saturation);
-	cap_device.setSharpness(sharpness);
+    cap_device.setHue(hue);
+    cap_device.setBrightness(brightness);
+    cap_device.setContrast(contrast);
+    cap_device.setSaturation(saturation);
+    cap_device.setSharpness(sharpness);
 
-	ROS_INFO("Min-Max brightness: %d-%d",cap_device.minBrightness(),cap_device.maxBrightness());
-	ROS_INFO("Min-Max contrast: %d-%d",cap_device.minContrast(),cap_device.maxContrast());
-	ROS_INFO("Min-Max saturation: %d-%d",cap_device.minSaturation(),cap_device.maxSaturation());
-	ROS_INFO("Min-Max hue: %d-%d",cap_device.minHue(),cap_device.maxHue());
-	ROS_INFO("Min-Max sharpness: %d-%d",cap_device.minSharpness(),cap_device.maxSharpness());
+    ROS_INFO("Min-Max brightness: %d-%d",cap_device.minBrightness(),cap_device.maxBrightness());
+    ROS_INFO("Min-Max contrast: %d-%d",cap_device.minContrast(),cap_device.maxContrast());
+    ROS_INFO("Min-Max saturation: %d-%d",cap_device.minSaturation(),cap_device.maxSaturation());
+    ROS_INFO("Min-Max hue: %d-%d",cap_device.minHue(),cap_device.maxHue());
+    ROS_INFO("Min-Max sharpness: %d-%d",cap_device.minSharpness(),cap_device.maxSharpness());
 
-	while ( ros::ok() )
-	{
-		cap_device.Get() == 0 ? cap_status = 0 : cap_status = 1;	
-		
-		if( cap_status > 0 )
-		{
-			try
-			{	
-				//convert using cv_bridge and publish
-				cap_device.toIplImage(rawImg);
-				cameraPub.publish( bridge_.cvToImgMsg( rawImg ) );
-			}
-			catch ( sensor_msgs::CvBridgeException error )
-			{
-				ROS_ERROR( "error converting IplImage with CvBridge" );
-			}
-		}
-		else
-		{
-			ROS_ERROR( "could not grab a frame from camera" );
-		}
-	
-		loop_rate.sleep();
-	}
+    while ( ros::ok() )
+    {
+        cap_device.Get() == 0 ? cap_status = 0 : cap_status = 1;    
+        
+        if( cap_status > 0 )
+        {
+            try
+            {   
+                //convert using cv_bridge and publish
+                cap_device.toIplImage(rawImg);
+                cameraPub.publish( bridge_.cvToImgMsg( rawImg ) );
+            }
+            catch ( sensor_msgs::CvBridgeException error )
+            {
+                ROS_ERROR( "error converting IplImage with CvBridge" );
+            }
+        }
+        else
+        {
+            ROS_ERROR( "could not grab a frame from camera" );
+        }
+    
+        loop_rate.sleep();
+    }
 
     return 0;
 }
